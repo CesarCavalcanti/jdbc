bf3ed534563d625af7501eb16981c79f6d51b5f5
CesarCavalcanti <senadevtrd@gmail.com>
2022-01-08 15:21:55 -0300

    Transações com JDBC

--- src/application/DeletarDados.java
+++ src/application/DeletarDados.java
@@ -1,5 +1,36 @@
 package application;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import db.DB;
+import db.DbIntergrityException;
+
 public class DeletarDados {
+	
+public static void main(String[] args) {
+		
+		//Atualizar valores no banco de dados
+		
+		Connection conn = null;
+		PreparedStatement st = null;
+		
+		try {
+			conn = DB.getConnection();
+			st = conn.prepareStatement("DELETE FROM department"
+					+"WHERE"
+					+"Id = ?");
+			st.setInt(1, 2);
+			
+			st.executeUpdate();
+					
+		}catch (SQLException e) {
+			throw new DbIntergrityException(e.getMessage());
+		}finally {
+			DB.closeStatement(st);
+			DB.closeConnection();
+		}
+}
 
 }
